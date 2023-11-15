2dbe1384d2df8552eb32ffde27bcac5616badec7
Ivan Samozhenov <i_dont_known@mail.ru>
2023-11-15 23:35:41 +0300

    Rewrite login page using loginForm component, add bootstrap layout

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
 import LoginForm from '../Components/LoginForm';
+import totaImg from '../img/Tota-with-flag.jpg';
 
 const Login = () => (
-  <LoginForm />
+  <div className="d-flex flex-column h-100">
+    <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">навбар</nav>
+    <div className="container-fluid h-100">
+      <div className="row justify-content-center align-content-center h-100">
+        <div className="col-12 col-md-8 col-xxl-6">
+          <div className="card shadow-sm">
+            <div className="card-body row p-5">
+              <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
+                <Image src={totaImg} alt="Tota on top holds flag" roundedCircle />
+              </div>
+              <LoginForm />
+            </div>
+            <div className="card-footer p-4">
+              <div className="text-center">
+                <span>Нет аккаунта?</span>
+                <a href="/signup">Регистрация</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 export default Login;
