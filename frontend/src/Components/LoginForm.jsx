d23460379e102d9bacfb606ee5c2fd6009b0b300
Ivan Samozhenov <i_dont_known@mail.ru>
2023-11-16 23:11:18 +0300

    Add styles to LoginForm component

--- frontend/src/Components/LoginForm.jsx
+++ frontend/src/Components/LoginForm.jsx
@@ -24,35 +24,44 @@ const LoginForm = () => {
       className="col-12 col-md-6 mt-3 mt-mb-0"
       onSubmit={formik.handleSubmit}
     >
-      <h1>Войти</h1>
-      <Form.Group>
+      <h1 className="text-center mb-4">Войти</h1>
+      <Form.Group className="form-floating mb-3">
         <Form.Control
           type="text"
           autoComplete="username"
           name="nickname"
           id="nickname"
-          placeholder="Ваш никнейм"
+          placeholder="Ваш ник"
+          required
           onChange={formik.handleChange}
           value={formik.values.nickname}
+          // isInvalid
         />
+        <Form.Label htmlFor="nickname">Ваш ник</Form.Label>
         {formik.errors.nickname && formik.touched.nickname
           ? <div>{formik.errors.nickname}</div>
           : null}
       </Form.Group>
-      <Form.Group className="form-group">
+      <Form.Group className="form-floating mb-4">
         <Form.Control
           type="password"
           name="password"
           id="password"
           placeholder="Пароль"
+          required
           onChange={formik.handleChange}
           value={formik.values.password}
+          // isInvalid
         />
+        <Form.Label htmlFor="password">Пароль</Form.Label>
+        <Form.Control.Feedback type="invalid" tooltip>
+          Неверные имя пользователя или пароль
+        </Form.Control.Feedback>
         {formik.touched.password && formik.errors.password
           ? <div>{formik.errors.password}</div>
           : null}
       </Form.Group>
-      <Button type="submit">Войти</Button>
+      <Button type="submit" variant="outline-primary" className="w-100 mb-3">Войти</Button>
     </Form>
   );
 };
