a6e9d6f87822ece1f783d0cb7c336298fcf20fe9
Ivan Samozhenov <i_dont_known@mail.ru>
2023-11-18 22:15:09 +0300

    Add authentification functionality Also updated path handling: this now checks if there is a local storage token

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,66 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from './Pages/Main';
-import LoginPage from './Pages/Login';
-import NotFoundPage from './Pages/NotFound';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router, Routes, Route, Navigate,
+} from 'react-router-dom';
+import useAuth from './hooks/index';
+import AuthContext from './contexts/index';
+import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
+import NotFoundPage from './pages/NotFoundPage';
+
+const AuthProvider = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(() => !!localStorage.getItem('userId'));
+  const logIn = () => setLoggedIn(true);
+  const logOut = () => {
+    localStorage.removeItem('userId');
+    setLoggedIn(false);
+  };
+  return (
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
+    <AuthContext.Provider value={{ loggedIn, logIn, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const MainRoute = ({ children }) => {
+  const auth = useAuth();
+  return (
+    auth.loggedIn ? children : <Navigate to="/login" />
+  );
+};
+
+const LoginRoute = ({ children }) => {
+  const auth = useAuth();
+  return (
+    !auth.loggedIn ? children : <Navigate to="/" />
+  );
+};
 
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="*" element={<NotFoundPage />} />
-      <Route path="/" element={<MainPage />} />
-      <Route path="login" element={<LoginPage />} />
-    </Routes>
-  </BrowserRouter>
+  <AuthProvider>
+    <Router>
+      <Routes>
+        <Route path="*" element={<NotFoundPage />} />
+        <Route
+          path="/"
+          element={(
+            <MainRoute>
+              <MainPage />
+            </MainRoute>
+          )}
+        />
+        <Route
+          path="/login"
+          element={(
+            <LoginRoute>
+              <LoginPage />
+            </LoginRoute>
+          )}
+        />
+      </Routes>
+    </Router>
+  </AuthProvider>
 );
 
 export default App;
