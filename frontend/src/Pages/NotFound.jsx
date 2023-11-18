989165fec689339f29fbec8beff6f36ba3232bd9
Ivan Samozhenov <i_dont_known@mail.ru>
2023-11-18 22:05:50 +0300

    Rename 404 page, add link to Mainpage

--- frontend/src/Pages/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const BuildPage = () => (
-  <div>
-    <h1>А тут 404</h1>
-  </div>
-);
-
-export default BuildPage;
---
+++ frontend/src/pages/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFoundPage = () => (
+  <div>
+    <h1>А тут 404</h1>
+    <a href="/">Вернуться на главную</a>
+  </div>
+);
+
+export default NotFoundPage;
