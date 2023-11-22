a66e3441da249742ca4fe9f1d2bf5fa18ceda9db
Ivan Samozhenov <i_dont_known@mail.ru>
2023-11-22 22:54:51 +0300

    Add sidebar component (layout)

--- frontend/src/components/ChatChannels.jsx
+++ frontend/src/components/ChatChannels.jsx
@@ -1,8 +1,38 @@
-import { Button, Nav, NavDropdown } from 'react-bootstrap';
+import {
+  Button,
+  ButtonGroup,
+  Nav,
+  Dropdown,
+} from 'react-bootstrap';
 import { PlusSquare } from 'react-bootstrap-icons';
 
-const ChatChannels = () => {
+const generateChannelButton = (name, removable, variant) => {
+  const ChannelButton = (
+    <Button variant={variant} className="w-100 rounded-0 text-start text-truncate">
+      <span className="me-1">#</span>
+      {name}
+    </Button>
+  );
+
+  if (!removable) {
+    return ChannelButton;
+  }
+
+  return (
+    <Dropdown className="d-flex dropdown btn-group" as={ButtonGroup}>
+      {ChannelButton}
+      <Dropdown.Toggle split variant="light" id="dropdown-split-basic" />
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Удалить</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Переименовать</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
+
+const ChatChannels = ({ channels }) => {
   const handleChannelSelect = () => console.log('handleChannelSelect');
+
   return (
     <>
       <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
@@ -12,29 +42,20 @@ const ChatChannels = () => {
           <span className="visually-hidden">+</span>
         </Button>
       </div>
-      <Nav as="ul" className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block" fill variant="tabs" defaultActiveKey="1" onClick={handleChannelSelect}>
-        <Nav.Item as="li">
-          <Button eventKey="1" variant="secondary" className="w-100 rounded-0 text-start">
-            <span className="me-1">#</span>
-            general
-          </Button>
-        </Nav.Item>
-        <Nav.Item as="li" className="nav-item w-100">
-          <Button className="w-100 rounded-0 text-start">
-            <span className="me-1">#</span>
-            random
-          </Button>
-        </Nav.Item>
-        <Nav.Item as="li">
-          <Nav.Link eventKey="3">Link</Nav.Link>
-        </Nav.Item>
-        <NavDropdown title="Dropdown" id="nav-dropdown">
-          <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
-          <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item eventKey="4.3">Something else here</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
+      <Nav as="ul" className="flex-column nav-pills px-2 mb-3 overflow-auto h-100 d-block" fill defaultActiveKey="1" onClick={handleChannelSelect}>
+        {channels.map(({
+          id,
+          name,
+          removable,
+          active,
+        }) => {
+          const variant = active ? 'secondary' : 'light';
+          return (
+            <Nav.Item as="li" key={id}>
+              {generateChannelButton(name, removable, variant)}
+            </Nav.Item>
+          );
+        })}
       </Nav>
     </>
   );
