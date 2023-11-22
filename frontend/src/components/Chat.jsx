4279967299fea9b6f84d0546d054c7f7843a0496
Ivan Samozhenov <i_dont_known@mail.ru>
2023-11-22 22:56:00 +0300

    Remove test links from MainPage, add dummy data for generating channels list

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,12 +1,35 @@
 import ChatChannels from './ChatChannels';
 
+const channels = [
+  {
+    id: 1,
+    name: 'General',
+    messages: [],
+    removable: false,
+    active: true,
+  },
+  {
+    id: 2,
+    name: 'Random',
+    messages: [],
+    removable: false,
+    active: false,
+  },
+  {
+    id: 3,
+    name: 'Music',
+    messages: [],
+    removable: true,
+    active: false,
+  },
+];
 const Chat = () => {
-  const a = 'chat';
+  const a = '';
   return (
     <div className="container h-100 my-4 overflow-hidden rounded shadow">
       <div className="row h-100 bg-white flex-md-row">
         <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
-          <ChatChannels />
+          <ChatChannels channels={channels} />
           {a}
         </div>
       </div>
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,25 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Chat from '../components/Chat';
 
 const MainPage = () => (
-  <div className="p-0">
+  <div className="d-flex flex-column h-100">
     <Navbar />
     <Chat />
-    <div className="d-flex flex-column h-100 p-5">
-      <h1>Корневая страница</h1>
-      <ul>
-        <li>
-          <Link to="/login">Странница залогиниваний</Link>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <Link to="/chat">Странница чата в разработке</Link>
-        </li>
-      </ul>
-    </div>
   </div>
 );
 
