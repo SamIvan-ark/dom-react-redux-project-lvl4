17d55ad192f10e15adce88edc5956e6e5d96fb2c
Ivan Samozhenov <i_dont_known@mail.ru>
2023-11-24 00:10:27 +0300

    Add main area layout, add test data (messages)

--- frontend/src/components/chat/Chat.jsx
+++ frontend/src/components/chat/Chat.jsx
@@ -1,10 +1,15 @@
 import ChannelsListArea from './ChannelsListArea';
+import MainArea from './MainArea';
 
 const channels = [
   {
     id: 1,
     name: 'General',
-    messages: [],
+    messages: [
+      { author: 'aboba', text: 'lalala' },
+      { author: 'bobi', text: 'alalala' },
+      { author: 'goga', text: 'olololo' },
+    ],
     removable: false,
     active: true,
   },
@@ -31,6 +36,9 @@ const Chat = () => {
         <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
           <ChannelsListArea channels={channels} />
           {a}
+        </div>
+        <div className="col p-0 h-100">
+          <MainArea channel={channels[0]} />
         </div>
       </div>
     </div>
---
+++ frontend/src/components/chat/MainArea.jsx
@@ -0,0 +1,54 @@
+import { Form, Button, InputGroup } from 'react-bootstrap';
+import { useFormik } from 'formik';
+import { ArrowRightSquare } from 'react-bootstrap-icons';
+
+const MainArea = ({ channel }) => {
+  const formik = useFormik({
+    initialValues: {
+      message: '',
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+  return (
+    <div className="d-flex flex-column h-100">
+      <div className="bg-light mb-4 p-3 shadow-sm small">
+        <p className="m-0">
+          <b>{`# ${channel.name}`}</b>
+        </p>
+        <span className="text-muted">{`${channel.messages.length} сообщений`}</span>
+      </div>
+      <div id="messages-box" className="chat-messages overflow-auto px-5">
+        {channel.messages.map((message) => (
+          <div key={message.id} className="text-break mb-2">
+            <b>{message.author}</b>
+            {`: ${message.text}`}
+          </div>
+        ))}
+      </div>
+      <div className="mt-auto px-5 py-3">
+        <Form className="py-1 border rounded-2">
+          <InputGroup hasValidation>
+            <Form.Control
+              autoFocus
+              type="text"
+              name="message"
+              id="message"
+              placeholder="Введите сообщение..."
+              required
+              onChange={formik.handleChange}
+              value={formik.values.message}
+            />
+            <Button type="button" variant="group-vertical">
+              <ArrowRightSquare size={20} />
+              <span className="visually-hidden">+</span>
+            </Button>
+          </InputGroup>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default MainArea;
