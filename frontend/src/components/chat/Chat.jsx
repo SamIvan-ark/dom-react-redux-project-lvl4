c43bbd5eec8470b6db2b88d89ed635b5f7766011
Ivan Samozhenov <i_dont_known@mail.ru>
2023-11-29 22:54:40 +0300

    Update testing data (messages at Chat.jsx)

--- frontend/src/components/chat/Chat.jsx
+++ frontend/src/components/chat/Chat.jsx
@@ -13,20 +13,6 @@ const channels = [
     removable: false,
     active: true,
   },
-  {
-    id: 2,
-    name: 'Random',
-    messages: [],
-    removable: false,
-    active: false,
-  },
-  {
-    id: 3,
-    name: 'Music',
-    messages: [],
-    removable: true,
-    active: false,
-  },
 ];
 const Chat = () => {
   const a = '';
@@ -34,7 +20,7 @@ const Chat = () => {
     <div className="container h-100 my-4 overflow-hidden rounded shadow">
       <div className="row h-100 bg-white flex-md-row">
         <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
-          <ChannelsListArea channels={channels} />
+          <ChannelsListArea />
           {a}
         </div>
         <div className="col p-0 h-100">
