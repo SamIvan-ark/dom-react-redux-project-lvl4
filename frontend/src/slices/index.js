b242ad367592659a7c7ebe1692c7818d240572ad
Ivan Samozhenov <i_dont_known@mail.ru>
2023-11-29 22:48:52 +0300

    Add modals Redux slice, update ./slices/index.js

--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import channelsSlice from './channelsSlice';
+import modalsSlice from './modalsSlice';
 
 export default configureStore({
   reducer: {
     channels: channelsSlice,
+    modals: modalsSlice,
   },
 });
