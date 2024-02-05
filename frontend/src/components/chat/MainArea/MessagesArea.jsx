de78c3b655cb128350b0b8c58692352e6f0f4e15
Ivan Samozhenov <i_dont_known@mail.ru>
2024-02-05 11:43:39 +0300

    Move logic of messages filtering from MessagesArea to parent

--- frontend/src/components/chat/MainArea/MessagesArea.jsx
+++ frontend/src/components/chat/MainArea/MessagesArea.jsx
@@ -1,36 +1,24 @@
 import { useRef, useEffect } from 'react';
-import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 
-import { useGetMessagesQuery } from '../../../api/messagesApi';
-import CenteredSpinner from '../../CenteredSpinner';
-
-const MessagesArea = () => {
+const MessagesArea = ({ messages, channel }) => {
   const { t } = useTranslation();
-  const currentChannel = useSelector((state) => state.ui.channels.activeChannel);
   const lastItemRef = useRef();
-  const { data: messages } = useGetMessagesQuery();
-  const currentMessages = !messages ? [] : messages // сомнительно
-    .filter((message) => message.channelId === currentChannel.id);
-
-  const isLoading = !messages || !currentChannel;
 
   useEffect(() => {
-    if (!isLoading) {
-      lastItemRef.current.scrollIntoView();
-    }
-  }, [isLoading, messages]);
+    lastItemRef.current.scrollIntoView();
+  }, [messages, channel]);
 
-  return (isLoading) ? <CenteredSpinner /> : (
+  return (
     <>
       <div className="bg-light mb-4 p-3 shadow-sm small">
         <p className="m-0">
-          <b>{`# ${currentChannel.name}`}</b>
+          <b>{`# ${channel.name}`}</b>
         </p>
-        <span className="text-muted">{t('entities.messages.count', { count: currentMessages.length })}</span>
+        <span className="text-muted">{t('entities.messages.count', { count: messages.length })}</span>
       </div>
       <div id="messages-box" className="chat-messages overflow-auto px-5">
-        {currentMessages.map((message) => (
+        {messages.map((message) => (
           <div key={message.id} className="text-break mb-2">
             <b>{message.author}</b>
             {`: ${message.text}`}
--- frontend/src/components/chat/MainArea/index.jsx
+++ frontend/src/components/chat/MainArea/index.jsx
@@ -1,11 +1,26 @@
+import { useSelector } from 'react-redux';
+
+import CenteredSpinner from '../../CenteredSpinner';
 import MessagesArea from './MessagesArea';
 import MessageInput from './MessageInput';
+import { useGetMessagesQuery } from '../../../api/messagesApi';
 
-const MainArea = () => (
-  <div className="d-flex flex-column h-100">
-    <MessagesArea />
-    <MessageInput />
-  </div>
-);
+const MainArea = () => {
+  const currentChannel = useSelector((state) => state.ui.channels.activeChannel);
+  const { data: messages } = useGetMessagesQuery();
+  const currentMessages = !messages ? [] : messages // сомнительно
+    .filter((message) => message.channelId === currentChannel.id);
+
+  const isLoading = !messages || !currentChannel;
+
+  return (
+    <div className="d-flex flex-column h-100">
+      {isLoading
+        ? <CenteredSpinner />
+        : <MessagesArea messages={currentMessages} channel={currentChannel} />}
+      <MessageInput />
+    </div>
+  );
+};
 
 export default MainArea;
