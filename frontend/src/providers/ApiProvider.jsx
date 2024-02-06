1378b85123c127201503d7266ac7fbadd71e0afd
Ivan Samozhenov <i_dont_known@mail.ru>
2024-02-06 16:57:08 +0300

    Remove message slice from store, remove unusual handlers from ApiProvider

--- frontend/src/providers/ApiProvider.jsx
+++ frontend/src/providers/ApiProvider.jsx
@@ -2,68 +2,18 @@ import { createContext, useContext, useMemo } from 'react';
 import { io } from 'socket.io-client';
 import { useTranslation } from 'react-i18next';
 
-import toasts from '../utils/toasts';
-
 const ApiContext = createContext({});
 
 export const useApi = () => useContext(ApiContext);
-
-const EVENT_NAMES = {
-  NEW_MESSAGE: 'newMessage',
-  NEW_CHANNEL: 'newChannel',
-  REMOVE_CHANNEL: 'removeChannel',
-  RENAME_CHANNEL: 'renameChannel',
-};
-
-const WAITING_FOR_ACKNOWLEDGE_MS = 5000;
-
-const handleEmittingResults = (cb, t) => (err, response) => {
-  if (err) {
-    if (err.message === 'operation has timed out') {
-      toasts.error(t('errors.networkError'));
-    }
-  }
-  cb(err, response);
-};
 
 export const ApiProvider = ({ children }) => {
   const socket = io({
     autoConnect: false,
   });
   const { t } = useTranslation();
-  const value = useMemo(() => {
-    const newMessage = (data, cb) => {
-      socket
-        .timeout(WAITING_FOR_ACKNOWLEDGE_MS)
-        .emit(EVENT_NAMES.NEW_MESSAGE, data, handleEmittingResults(cb, t));
-    };
-
-    const newChannel = (data, cb) => {
-      socket
-        .timeout(WAITING_FOR_ACKNOWLEDGE_MS)
-        .emit(EVENT_NAMES.NEW_CHANNEL, data, handleEmittingResults(cb, t));
-    };
-
-    const removeChannel = (data, cb) => {
-      socket
-        .timeout(WAITING_FOR_ACKNOWLEDGE_MS)
-        .emit(EVENT_NAMES.REMOVE_CHANNEL, data, handleEmittingResults(cb, t));
-    };
-
-    const renameChannel = (data, cb) => {
-      socket
-        .timeout(WAITING_FOR_ACKNOWLEDGE_MS)
-        .emit(EVENT_NAMES.RENAME_CHANNEL, data, handleEmittingResults(cb, t));
-    };
-
-    return {
-      newMessage,
-      newChannel,
-      removeChannel,
-      renameChannel,
-      socket,
-    };
-  }, [socket, t]);
+  const value = useMemo(() => ({
+    socket,
+  }), [socket, t]);
 
   return (
     <ApiContext.Provider value={value}>
--- frontend/src/slices/index.js
+++ frontend/src/slices/index.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import messagesSlice from './messagesSlice';
 import channelsApi from '../api/channelsApi';
 import userApi from '../api/userApi';
 import uiSlice from './uiSlice';
@@ -8,7 +7,6 @@ import messagesApi from '../api/messagesApi';
 export default configureStore({
   reducer: {
     ui: uiSlice,
-    messages: messagesSlice,
     [channelsApi.reducerPath]: channelsApi.reducer,
     [userApi.reducerPath]: userApi.reducer,
     [messagesApi.reducerPath]: messagesApi.reducer,
